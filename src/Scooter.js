b2ff946922655e8e5e3f133256887b71ab5c641c
AdrianBurke1 <adrianburke902@gmail.com>
2024-09-23 15:11:12 -0400

    update

--- src/Scooter.js
+++ src/Scooter.js
@@ -8,6 +8,41 @@ class Scooter {
     this.charge = 100;
     this.isBroken = fales;
   }
+  rent(user){
+    if(this.charge > 20 && !this.isBroken){
+      this.station = null;
+      this.user = user;
+      console.log(`Scooter ${this.serial} rented to ${user}`);
+    } else if(this.charge <= 20){
+      throw new Error('Scooter needs to charge');
+    } else if(this.isBroken) {
+      throw new Error('Scooter needs repair')
+    }
+  }
+   dock(station){
+    this.station = station;
+    this.user = null;
+    console.log(`Scooter ${this.serial} docked at ${station}`);
+   }
+   recharge(){
+    console.log('Recharging scooter...');
+    const rechargeIntervale = setInterval(() => {
+      if(this.charge < 100){
+        this.charge += 10;
+        console.log(`Charge: ${this.charge}%`);
+      } else {
+        console.log('Scooter fully charged');
+        clearInterval(rechargeIntervale);
+      }
+    }, 1000)
+   }
+   requestRepair(){
+    console.log('Repair requested...');
+    setTimeout(() => {
+      this.isBroken = false;
+      console.log(`Scooter ${this.serial} repaired`)
+    }, 5000)
+   }
 }
 
 module.exports = Scooter
