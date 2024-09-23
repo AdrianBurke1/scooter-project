5725b18f954e14714af7c0fd28021de47e884eb7
AdrianBurke1 <adrianburke902@gmail.com>
2024-09-23 16:11:51 -0400

    user

--- src/User.js
+++ src/User.js
@@ -1,5 +1,23 @@
 class User {
   // User code here
+  constructor(username, password, age) {
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = false;
+  }
+
+  login(password){
+    if(this.password === true){
+      console.log(`${this.username} has logged in successfully.`)
+    } else {
+      throw Error('Incorrect password')
+    }
+  }
+   logout(){
+    this.loggedIn = false;
+    console.log(`${this.username} has logged out.`);
+   }
 }
 
 module.exports = User
